e95e25c313d23a0e5e17fbae33f57089b9e10f49
akramaskar <akramaskar@gmail.com>
2021-09-03 12:12:44 +0200

    initial commit

---
+++ android/src/main/kotlin/com/xraph/plugin/flutter_unity_widget/FlutterUnityWidgetOptions.kt
@@ -0,0 +1,5 @@
+package com.xraph.plugin.flutter_unity_widget
+
+class FlutterUnityWidgetOptions {
+    var fullscreenEnabled: Boolean = false
+}
---
+++ android/src/main/kotlin/com/xraph/plugin/flutter_unity_widget/FlutterUnityWidgetOptionsSink.kt
@@ -0,0 +1,5 @@
+package com.xraph.plugin.flutter_unity_widget
+
+interface FlutterUnityWidgetOptionsSink {
+    fun setFullscreenEnabled(fullscreenEnabled: Boolean)
+}
